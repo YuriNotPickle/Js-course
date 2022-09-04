923d775699b3f05375e649edc84f77da5d944c83
Yuriy Klimenko <yurriyklimenko@gmail.com>
2022-09-04 13:01:53 +0300

    Lesson 6 done

---
+++ Lesson 6/js/script.js
@@ -0,0 +1,60 @@
+window.addEventListener('load', function(){
+   class Slider {
+      constructor(el) {
+         this.element = document.querySelector(el);
+         this.images = this.element.querySelectorAll('.photos img');
+         this.btnNext = this.element.querySelector('.buttons .next');
+         this.btnPrev = this.element.querySelector('.buttons .prev');
+         this.currentImg = 0;
+         this.init();
+      }
+
+      removeClass(val) {
+         this.images[this.currentImg].classList.remove(val);
+      }
+
+      addClass(val) {
+         this.images[this.currentImg].classList.add(val);
+      }
+
+      prev() {
+         this.removeClass('showed')
+         this.currentImg--;
+
+         if (this.currentImg < 0) this.currentImg = this.images.length - 1;
+
+         this.addClass('showed')
+      }
+
+      next() {
+         this.removeClass('showed')
+         this.currentImg++;
+
+         if(this.currentImg >= this.images.length) this.currentImg = 0;
+
+         this.addClass('showed')
+      }
+
+      init() {
+         this.btnNext.addEventListener('click', () => this.next());
+         this.btnPrev.addEventListener('click', () => this.prev());
+      }
+   }
+   new Slider('.gallery-1');
+
+   let slider2 = new Slider('.gallery-2');
+   setInterval(function(){
+		slider2.next();
+	}, 3000);
+
+});
+
+/*
+new Slider('.gallery-1');
+new Slider('.gallery-2'); 
+
+*/
+
+/* this.btn.addEventListener('click', () => {
+	this.next()
+}) */
